274a10b1cc0006edf0a11854381d34e26ba3690a
indexpro24@gmail.com <indexpro24@gmail.com>
2022-09-10 21:49:12 +0300

    i

--- app/src/components/Layouts/Navigation.js
+++ app/src/components/Layouts/Navigation.js
@@ -22,20 +22,20 @@ const Navigation = ({ user }) => {
                 <div className="flex justify-between h-16">
                     <div className="flex">
                         {/* Logo */}
-                        <div className="flex-shrink-0 flex items-center">
+                        {/* <div className="flex-shrink-0 flex items-center">
                             <Link href="/dashboard">
                                 <a>
-                                    <ApplicationLogo className="block h-10 w-auto fill-current text-gray-600" />
+                                    <ApplicationLogo className="block h-8 w-auto fill-current text-gray-600" />
                                 </a>
                             </Link>
-                        </div>
+                        </div> */}
 
                         {/* Navigation Links */}
                         <div className="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                             <NavLink
                                 href="/dashboard"
                                 active={router.pathname === '/dashboard'}>
-                                Dashboard
+                                Дашборд
                             </NavLink>
                         </div>
                     </div>
@@ -111,7 +111,7 @@ const Navigation = ({ user }) => {
                         <ResponsiveNavLink
                             href="/dashboard"
                             active={router.pathname === '/dashboard'}>
-                            Dashboard
+                            Дашборд
                         </ResponsiveNavLink>
                     </div>
 
