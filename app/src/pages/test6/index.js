023c66eb1cb9af193615b00206f6399f602d5cab
indexpro24@gmail.com <indexpro24@gmail.com>
2022-09-14 18:11:04 +0300

    i

--- app/src/pages/test6/index.js
+++ app/src/pages/test6/index.js
@@ -1,9 +1,15 @@
-import AppLayout from '@/components/Layouts/AppLayout'
-import Head from 'next/head'
-import { useQuery } from '@apollo/client'
-import React, { useEffect, useRef } from 'react'
+import React from 'react'
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js'
+import { Doughnut } from 'react-chartjs-2'
 import Chart from 'chart.js/auto'
-import { Bar } from 'react-chartjs-2'
 
 const data = {
     labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
@@ -33,92 +39,5 @@ const data = {
 }
 
 export default function App() {
-    const canvasEl = useRef(null)
-
-    const colors = {
-        purple: {
-            default: 'rgba(149, 76, 233, 1)',
-            half: 'rgba(149, 76, 233, 0.5)',
-            quarter: 'rgba(149, 76, 233, 0.25)',
-            zero: 'rgba(149, 76, 233, 0)',
-        },
-        indigo: {
-            default: 'rgba(80, 102, 120, 1)',
-            quarter: 'rgba(80, 102, 120, 0.25)',
-        },
-    }
-
-    useEffect(() => {
-        const ctx = canvasEl.current.getContext('2d')
-        // const ctx = document.getElementById("myChart");
-
-        const gradient = ctx.createLinearGradient(0, 16, 0, 600)
-        gradient.addColorStop(0, colors.purple.half)
-        gradient.addColorStop(0.65, colors.purple.quarter)
-        gradient.addColorStop(1, colors.purple.zero)
-
-        const weight = [5, 8, 3, 77, 24, 12]
-
-        const labels = [
-            'Week 1',
-            'Week 2',
-            'Week 3',
-            'Week 4',
-            'Week 5',
-            'Week 6',
-            'Week 7',
-            'Week 8',
-            'Week 9',
-            'Week 10',
-        ]
-        const data = {
-            labels: labels,
-            datasets: [
-                {
-                    backgroundColor: gradient,
-                    label: 'My First Dataset',
-                    data: weight,
-                    fill: true,
-                    borderWidth: 2,
-                    borderColor: colors.purple.default,
-                    lineTension: 0.2,
-                    pointBackgroundColor: colors.purple.default,
-                    pointRadius: 3,
-                },
-            ],
-        }
-        const config = {
-            type: 'line',
-            data: data,
-        }
-        const myLineChart = new Chart(ctx, config)
-
-        return function cleanup() {
-            myLineChart.destroy()
-        }
-    })
-
-    return (
-        <AppLayout
-            header={
-                <h2 className="font-semibold text-xl text-gray-800 leading-tight">
-                    Тест4
-                </h2>
-            }>
-            <Head>
-                <title>Тест4</title>
-            </Head>
-            <div>
-                <h2>Bar Example (custom size)</h2>
-                <Bar
-                    data={data}
-                    width={400}
-                    height={200}
-                    options={{
-                        maintainAspectRatio: false,
-                    }}
-                />
-            </div>
-        </AppLayout>
-    )
+    return <Doughnut data={data} />
 }
