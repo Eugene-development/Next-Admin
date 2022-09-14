e90c086f1c583b2f173ac53bcd7ebe5fad4a3612
indexpro24@gmail.com <indexpro24@gmail.com>
2022-09-14 17:41:07 +0300

    i

--- app/src/pages/test6/index.js
+++ app/src/pages/test6/index.js
@@ -1,9 +1,36 @@
 import AppLayout from '@/components/Layouts/AppLayout'
 import Head from 'next/head'
 import { useQuery } from '@apollo/client'
-
 import React, { useEffect, useRef } from 'react'
 import Chart from 'chart.js/auto'
+import { Bar } from 'react-chartjs-2'
+
+const data = {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [
+        {
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+            ],
+            borderWidth: 1,
+        },
+    ],
+}
 
 export default function App() {
     const canvasEl = useRef(null)
@@ -72,9 +99,26 @@ export default function App() {
     })
 
     return (
-        <div className="App">
-            <span>Chart.js Demo</span>
-            <canvas id="myChart" ref={canvasEl} height="100" />
-        </div>
+        <AppLayout
+            header={
+                <h2 className="font-semibold text-xl text-gray-800 leading-tight">
+                    Тест4
+                </h2>
+            }>
+            <Head>
+                <title>Тест4</title>
+            </Head>
+            <div>
+                <h2>Bar Example (custom size)</h2>
+                <Bar
+                    data={data}
+                    width={400}
+                    height={200}
+                    options={{
+                        maintainAspectRatio: false,
+                    }}
+                />
+            </div>
+        </AppLayout>
     )
 }
