c1ece4fd87fc681cdbde219f02d2350ca1f4c336
indexpro24@gmail.com <indexpro24@gmail.com>
2022-09-14 20:51:28 +0300

    i

--- app/src/pages/seo/position/query/index.js
+++ app/src/pages/seo/position/query/index.js
@@ -1,50 +1,76 @@
-import React from 'react'
-import 'chart.js/auto'
-import { Line } from 'react-chartjs-2'
 import AppLayout from '@/components/Layouts/AppLayout'
 import Head from 'next/head'
+import { useQuery } from '@apollo/client'
+import React, { useEffect, useRef } from 'react'
+import Chart from 'chart.js/auto'
 
-const data = {
-    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-    datasets: [
-        {
-            label: 'My First dataset',
-            fill: false,
-            lineTension: 0.1,
-            backgroundColor: 'rgba(75,192,192,0.4)',
-            borderColor: 'rgba(75,192,192,1)',
-            borderCapStyle: 'butt',
-            borderDash: [],
-            borderDashOffset: 0.0,
-            borderJoinStyle: 'miter',
-            pointBorderColor: 'rgba(75,192,192,1)',
-            pointBackgroundColor: '#fff',
-            pointBorderWidth: 1,
-            pointHoverRadius: 5,
-            pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-            pointHoverBorderColor: 'rgba(220,220,220,1)',
-            pointHoverBorderWidth: 2,
-            pointRadius: 1,
-            pointHitRadius: 10,
-            data: [65, 59, 80, 81, 56, 55, 40],
+export default function App() {
+    const canvasEl = useRef(null)
+
+    const colors = {
+        purple: {
+            default: 'rgba(149, 76, 233, 1)',
+            half: 'rgba(149, 76, 233, 0.5)',
+            quarter: 'rgba(149, 76, 233, 0.25)',
+            zero: 'rgba(149, 76, 233, 0)',
         },
-    ],
-}
+        indigo: {
+            default: 'rgba(80, 102, 120, 1)',
+            quarter: 'rgba(80, 102, 120, 0.25)',
+        },
+    }
 
-export default () => (
-    <AppLayout
-        header={
-            <h2 className="font-semibold text-xl text-gray-800 leading-tight">
-                Запрос
-            </h2>
-        }>
-        <Head>
-            <title>Запрос</title>
-        </Head>
+    useEffect(() => {
+        const ctx = canvasEl.current.getContext('2d')
+        // const ctx = document.getElementById("myChart");
 
-        <div className="">
-            <h2>Line Example</h2>
-            <Line data={data} width={400} height={150} />
+        const gradient = ctx.createLinearGradient(0, 16, 0, 600)
+        gradient.addColorStop(0, colors.purple.half)
+        gradient.addColorStop(0.65, colors.purple.quarter)
+        gradient.addColorStop(1, colors.purple.zero)
+
+        const position = [5, 8, 3, 77, 24, 12, 15]
+
+        const labels = [
+            'Сегодня',
+            'Вчера',
+            'Неделю назад',
+            'Месяц назад',
+            'Три месяца назад',
+            'Шесть месяцев назад',
+            'Год назад',
+        ]
+        const data = {
+            labels: labels,
+            datasets: [
+                {
+                    backgroundColor: gradient,
+                    label: 'Позиция',
+                    data: position,
+                    fill: true,
+                    borderWidth: 2,
+                    borderColor: colors.purple.default,
+                    lineTension: 0.2,
+                    pointBackgroundColor: colors.purple.default,
+                    pointRadius: 3,
+                },
+            ],
+        }
+        const config = {
+            type: 'line',
+            data: data,
+        }
+        const myLineChart = new Chart(ctx, config)
+
+        return function cleanup() {
+            myLineChart.destroy()
+        }
+    })
+
+    return (
+        <div className="App">
+            <span>Запрос</span>
+            <canvas id="myChart" ref={canvasEl} height="100" />
         </div>
-    </AppLayout>
-)
+    )
+}
