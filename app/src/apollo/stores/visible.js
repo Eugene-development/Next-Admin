70574187aa8fa22e3349fa7b88e0c4f8962ca46d
indexpro24@gmail.com <indexpro24@gmail.com>
2022-10-11 22:36:14 +0300

    c

--- app/src/apollo/stores/visible.js
+++ app/src/apollo/stores/visible.js
@@ -1,4 +1,6 @@
 import { makeVar } from '@apollo/client'
 
-export const is_visible_delete = makeVar(false)
 export const is_visible_create = makeVar(false)
+export const is_visible_read = makeVar(false)
+export const is_visible_update = makeVar(false)
+export const is_visible_delete = makeVar(false)
