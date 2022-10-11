17de151812385d0d6967d4df5374407540128e38
indexpro24@gmail.com <indexpro24@gmail.com>
2022-10-11 23:13:57 +0300

    c

--- app/src/components/CRUD/content/catalog/delete/DeleteItemCatalog.jsx
+++ app/src/components/CRUD/content/catalog/delete/DeleteItemCatalog.jsx
@@ -63,7 +63,7 @@ const DeleteItemCatalog = () => {
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
-              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
+              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-2xl sm:p-6">
                 <div className="sm:flex sm:items-start">
                   <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
                     <ExclamationTriangleIcon className="h-6 w-6 text-red-600" aria-hidden="true" />
@@ -74,7 +74,7 @@ const DeleteItemCatalog = () => {
                     </Dialog.Title>
                     <div className="mt-2">
                       <p className="text-sm text-gray-500">
-                        Элемент каталога "{currentValueCatalog}" будет удалён из базы данных. Если не уверены просто поменяйте переключателем статус элемента в таблице.
+                        Элемент каталога "<strong>{currentValueCatalog}</strong>" будет удалён из базы данных. Если вы не уверены просто поменяйте переключателем статус элемента в таблице.
                       </p>
                     </div>
                   </div>
