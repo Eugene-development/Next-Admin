8f3821af28b0cd25f06efc5c38a0460cb0e790c2
indexpro24@gmail.com <indexpro24@gmail.com>
2022-10-12 19:05:32 +0300

    c

--- app/src/components/CRUD/content/catalog/update/UpdateItemCatalog.jsx
+++ app/src/components/CRUD/content/catalog/update/UpdateItemCatalog.jsx
@@ -91,12 +91,12 @@ const handleAddCatalog = (e) => {
                     >
                     <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-2xl sm:p-6">
                         <div>
-                        <div className="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-green-100">
-                            <CheckIcon className="h-6 w-6 text-green-600" aria-hidden="true" />
+                        <div className="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-blue-100">
+                            <CheckIcon className="h-6 w-6 text-blue-600" aria-hidden="true" />
                         </div>
                         <div className="mt-3 text-center sm:mt-5">
                             <Dialog.Title as="h3" className="text-lg font-medium leading-6 text-gray-900">
-                            Добавление элемента каталога
+                            Обновление элемента каталога
                             </Dialog.Title>
                             <div className="mt-2">
                             <p className="text-sm text-gray-500">
@@ -112,7 +112,7 @@ const handleAddCatalog = (e) => {
                             <div className="mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6">
                                 <div className="sm:col-span-3">
                                 <label htmlFor="parent" className="block text-sm font-medium text-gray-700">
-                                    Принадлежит элементу меню
+                                    Изменить элемент меню
                                 </label>
                                 <div className="mt-1">
                                     <select
@@ -135,6 +135,7 @@ const handleAddCatalog = (e) => {
                                 </label>
                                 <div className="mt-1">
                                     <input
+                                    // value="jdiwedfnwifn"
                                     onChange={(e) => setText(e.target.value)}
                                     type="text"
                                     name="value"
@@ -153,15 +154,15 @@ const handleAddCatalog = (e) => {
                             <div className="mt-8 sm:mt-10 sm:grid sm:grid-flow-row-dense sm:grid-cols-2 sm:gap-3">
                                 <button
                                 type="submit"
-                                className="inline-flex w-full justify-center rounded-md border border-transparent bg-green-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 sm:col-start-2 sm:text-sm"
-                                onClick={() => is_visible_create(false)}
+                                className="inline-flex w-full justify-center rounded-md border border-transparent bg-blue-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 sm:col-start-2 sm:text-sm"
+                                onClick={() => is_visible_update(false)}
                                 >
-                                Добавить
+                                Обновить
                                 </button>
                                 <button
                                 type="button"
-                                className="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 sm:col-start-1 sm:mt-0 sm:text-sm"
-                                onClick={() => is_visible_create(false)}
+                                className="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 sm:col-start-1 sm:mt-0 sm:text-sm"
+                                onClick={() => is_visible_update(false)}
                                 ref={cancelButtonRef}
                                 >
                                 Отменить
