8454a55f83e6f65a1a6bfbcb553152b00da24db9
indexpro24@gmail.com <indexpro24@gmail.com>
2022-10-16 21:20:56 +0300

    c

---
+++ app/src/apollo/query/category.js
@@ -0,0 +1,90 @@
+import { gql } from '@apollo/client'
+
+export const ALL_CATEGORY = gql`
+    query all_category($key: String!) {
+        category(key: $key) {
+            id
+            value
+            is_active
+            created_at
+            updated_at
+            parent: parentable {
+                ... on Rubric {
+                    id
+                    value
+                }
+            }
+        }
+    }
+`
+
+export const ONE_CATEGORY = gql`
+    query category_one($id: ID!) {
+        category_one(id: $id) {
+            value
+            created_at
+            updated_at
+            parent: parentable {
+                ... on Rubric {
+                    value
+                }
+            }
+        }
+    }
+`
+
+export const CREATE_CATEGORY = gql`
+    mutation create_category(
+        $key: String!
+        $is_active: Boolean
+        $value: String!
+        $slug: String!
+        $parentableType: String
+        $parentableId: Int!
+    ) {
+        createCategory(
+            input: {
+                key: $key
+                is_active: $is_active
+                value: $value
+                slug: $slug
+                parentable_type: $parentableType
+                parentable_id: $parentableId
+            }
+        ) {
+            value
+        }
+    }
+`
+export const UPDATE_CATEGORY = gql`
+    mutation update_category(
+        $id: ID!
+        $key: String!
+        $is_active: Boolean
+        $value: String!
+        $slug: String
+        $parentableType: String
+        $parentableId: Int
+    ) {
+        updateCategory(
+            input: {
+                id: $id
+                key: $key
+                is_active: $is_active
+                value: $value
+                slug: $slug
+                parentable_type: $parentableType
+                parentable_id: $parentableId
+            }
+        ) {
+            value
+        }
+    }
+`
+export const DELETE_CATEGORY = gql`
+    mutation delete_category($id: ID!) {
+        deleteCategory(id: $id) {
+            value
+        }
+    }
+`
