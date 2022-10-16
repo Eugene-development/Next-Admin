995fed3da75c8b3cec33a517b93d484e272feb88
indexpro24@gmail.com <indexpro24@gmail.com>
2022-10-16 22:25:37 +0300

    с

--- app/src/apollo/stores/visible.js
+++ app/src/apollo/stores/visible.js
@@ -1,10 +1,5 @@
 import { makeVar } from '@apollo/client'
 
-// export const is_visible_create = makeVar(false)
-// export const is_visible_read = makeVar(false)
-// export const is_visible_update = makeVar(false)
-// export const is_visible_delete = makeVar(false)
-
 export const is_visible_create_menu = makeVar(false)
 export const is_visible_read_menu = makeVar(false)
 export const is_visible_update_menu = makeVar(false)
