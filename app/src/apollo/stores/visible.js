0740723cb344599410b7af5821147f6da8798d9a
indexpro24@gmail.com <indexpro24@gmail.com>
2022-10-16 19:07:16 +0300

    c

--- app/src/apollo/stores/visible.js
+++ app/src/apollo/stores/visible.js
@@ -1,6 +1,31 @@
 import { makeVar } from '@apollo/client'
 
-export const is_visible_create = makeVar(false)
-export const is_visible_read = makeVar(false)
-export const is_visible_update = makeVar(false)
-export const is_visible_delete = makeVar(false)
+// export const is_visible_create = makeVar(false)
+// export const is_visible_read = makeVar(false)
+// export const is_visible_update = makeVar(false)
+// export const is_visible_delete = makeVar(false)
+
+export const is_visible_create_menu = makeVar(false)
+export const is_visible_read_menu = makeVar(false)
+export const is_visible_update_menu = makeVar(false)
+export const is_visible_delete_menu = makeVar(false)
+
+export const is_visible_create_catalog = makeVar(false)
+export const is_visible_read_catalog = makeVar(false)
+export const is_visible_update_catalog = makeVar(false)
+export const is_visible_delete_catalog = makeVar(false)
+
+export const is_visible_create_rubric = makeVar(false)
+export const is_visible_read_rubric = makeVar(false)
+export const is_visible_update_rubric = makeVar(false)
+export const is_visible_delete_rubric = makeVar(false)
+
+export const is_visible_create_category = makeVar(false)
+export const is_visible_read_category = makeVar(false)
+export const is_visible_update_category = makeVar(false)
+export const is_visible_delete_category = makeVar(false)
+
+export const is_visible_create_product = makeVar(false)
+export const is_visible_read_product = makeVar(false)
+export const is_visible_update_product = makeVar(false)
+export const is_visible_delete_product = makeVar(false)
