c6c0916fbe722a41618c7859f1b3708f4baa1da9
Eugene Che <indexpro24@gmail.com>
2023-05-02 18:46:16 +0300

    c

--- app/src/components/CRUD/content/category/update/UpdateItemCategory.jsx
+++ app/src/components/CRUD/content/category/update/UpdateItemCategory.jsx
@@ -28,10 +28,8 @@ const UpdateItemCategory = () => {
 
     const currentSeoTitleCategory = useReactiveVar(current_seoTitle_category)
     const currentIdSeoTitleCategory = useReactiveVar(current_id_seoTitle_category)
-    console.log(currentIdSeoTitleCategory);
     const currentSeoDescriptionCategory = useReactiveVar(current_seoDescription_category)
     const currentIdSeoDescriptionCategory = useReactiveVar(current_id_seoDescription_category)
-        console.log(currentIdSeoDescriptionCategory);
 
     const currentIdCategory = useReactiveVar(current_id_category)
     const currentValueCategory = useReactiveVar(current_value_category)
